fd0c3b809c4a6b379eddcdc8758a06d6471bd93e
ursobank <seuemail@gmail.com>
2026-05-21 15:25:30 -0400

    fix gunicorn

--- app.py
+++ app.py
@@ -123,9 +123,5 @@ if __name__ == "__main__":
 
     port = int(os.environ.get("PORT", 3333))
 
-    import os
-
-app.run(
-    host="0.0.0.0",
-    port=int(os.environ.get("PORT", 8080))
-)
+    if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=3333)
