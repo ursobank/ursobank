881f66c3929bff20ffff4d2288eaa2a847534cb3
ursobank <seuemail@gmail.com>
2026-05-21 15:40:33 -0400

    app limpa

--- app.py
+++ app.py
@@ -1,107 +1,19 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template
 import os
 
 app = Flask(__name__)
 
-usuarios = {}
-
-saldo = {
-    "admin": 100
-}
-
 
 @app.route("/")
 def home():
-    return render_template("login.html")
+    return """
+    <h1>UrsoBank Online</h1>
+    <p>Servidor funcionando.</p>
 
-
-@app.route("/cadastro")
-def cadastro():
-    return render_template("cadastro.html")
-
-
-@app.route("/registrar", methods=["POST"])
-def registrar():
-
-    user = request.form["user"]
-    password = request.form["password"]
-
-    usuarios[user] = password
-
-    return redirect("/")
-
-
-@app.route("/login", methods=["POST"])
-def login():
-
-    user = request.form["user"]
-    password = request.form["password"]
-
-    if user in usuarios:
-
-        if usuarios[user] == password:
-
-            return redirect("/dashboard")
-
-    return "Login inválido"
-
-
-@app.route("/dashboard")
-def dashboard():
-    return render_template("dashboard.html")
-
-
-@app.route("/pix")
-def pix():
-    return render_template("pix.html")
-
-
-@app.route("/saque", methods=["GET", "POST"])
-def saque():
-
-    usuario = "admin"
-
-    if request.method == "POST":
-
-        valor = float(request.form["valor"])
-
-        if saldo[usuario] >= valor:
-
-            saldo[usuario] -= valor
-
-            return f"""
-            <body style='background:#050b1a;color:white;font-family:Arial;padding:30px'>
-
-            <h1>✅ Saque realizado</h1>
-
-            <h2>Valor: R$ {valor}</h2>
-
-            <h3>Saldo restante: R$ {saldo[usuario]}</h3>
-
-            <a href='/dashboard' style='color:#60a5fa'>
-            Voltar
-            </a>
-
-            </body>
-            """
-
-        else:
-
-            return """
-            <body style='background:#050b1a;color:white;font-family:Arial;padding:30px'>
-
-            <h1>❌ Saldo insuficiente</h1>
-
-            <h3>Você não possui saldo para sacar.</h3>
-
-            <a href='/saque' style='color:#60a5fa'>
-            Voltar
-            </a>
-
-            </body>
-            """
-
-    return render_template("saque.html")
+    <a href='/perfil'>Perfil</a><br>
+    <a href='/extrato'>Extrato</a><br>
+    <a href='/transferir'>Transferir</a>
+    """
 
 
 @app.route("/extrato")
@@ -119,7 +31,5 @@ def transferir():
     return render_template("transferir.html")
 
 
-
-
-   if __name__ == "__main__":
+if __name__ == "__main__":
     app.run(host="0.0.0.0", port=3333)
