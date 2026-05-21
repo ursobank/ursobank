5ac614b383a4436bbbed2ccee019d168a3bd4c5c
ursobank <seuemail@gmail.com>
2026-05-21 15:28:41 -0400

    remove duplicate app run

--- app.py
+++ app.py
@@ -123,5 +123,5 @@ if __name__ == "__main__":
 
     port = int(os.environ.get("PORT", 3333))
 
-    if __name__ == "__main__":
+   if __name__ == "__main__":
     app.run(host="0.0.0.0", port=3333)
