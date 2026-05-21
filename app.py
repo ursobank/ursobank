a39c187b34f9b097adf34c947a0c4be42c467eba
ursobank <seuemail@gmail.com>
2026-05-21 15:58:57 -0400

    app final

--- app.py
+++ app.py
@@ -3,33 +3,22 @@ import os
 
 app = Flask(__name__)
 
-
 @app.route("/")
 def home():
-    return """
-    <h1>UrsoBank Online</h1>
-    <p>Servidor funcionando.</p>
-
-    <a href='/perfil'>Perfil</a><br>
-    <a href='/extrato'>Extrato</a><br>
-    <a href='/transferir'>Transferir</a>
-    """
-
-
-@app.route("/extrato")
-def extrato():
-    return render_template("extrato.html")
-
+    return render_template("perfil.html")
 
 @app.route("/perfil")
 def perfil():
     return render_template("perfil.html")
 
+@app.route("/extrato")
+def extrato():
+    return render_template("extrato.html")
 
 @app.route("/transferir")
 def transferir():
     return render_template("transferir.html")
 
-
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=3333)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
