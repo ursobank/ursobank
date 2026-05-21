9969700e9aa90302842f609fa4433265531395fd
ursobank <seuemail@gmail.com>
2026-05-21 16:30:16 -0400

    novo app

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("perfil.html")
+    return "UrsoBank Online"
 
 @app.route("/perfil")
 def perfil():
@@ -27,24 +27,4 @@ def saque():
     return render_template("saque.html")
 
 @app.route("/login")
-def login():
-    return render_template("login.html")
-
-@app.route("/cadastro")
-def cadastro():
-    return render_template("cadastro.html")
-
-@app.route("/dashboard")
-def dashboard():
-    return render_template("dashboard.html")
-
-@app.route("/admin")
-def admin():
-    return render_template("admin.html")
-
-@app.route("/")
-def home():
-    return "UrsoBank Online"
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=3333)
+def
